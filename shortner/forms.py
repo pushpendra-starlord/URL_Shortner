7bd75d1563383e18abd32a909704484cfdddceac
Pushpendra Tripathi <pushpendratripathi@outlook.com>
2021-07-27 00:22:47 +0530

    bug fixes

--- shortner/forms.py
+++ shortner/forms.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 from django.db.models import fields
 from django.forms import ModelForm
 from django.contrib.auth.models import User
@@ -8,6 +9,12 @@ class ShortUrlForm(ModelForm):
         model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
 
+        widgets = {
+            # telling Django your password field in the mode is a password input on the template
+            'password': forms.PasswordInput() 
+        }
+
+    #making the password hashed by overiding the save() method
     def save(self, commit=True):
         user = super(ShortUrlForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password"])
